23e46bc8cbbfe9c38be2a32ab00e5410c9c1273d
hannahellis4242 <hannahellis4242@gmail.com>
2023-09-13 00:51:14 +0100

    Adding multiple post route

--- CouponServer/src/routes/routes.ts
+++ CouponServer/src/routes/routes.ts
@@ -5,6 +5,8 @@ import couponService from "../service/couponService";
 import { couponSchema } from "../model/Coupon";
 import Config from "../config";
 
+const couponArraySchema = z.array(couponSchema);
+
 const routes = (config: Config) => {
   const routes = Router();
 
@@ -30,14 +32,27 @@ const routes = (config: Config) => {
     }
   });
 
-  routes.get("/all", async (req, res) => {
+  routes.get("/all", async (_, res) => {
     try {
       const data = await readAll();
       if (data.length === 0) {
         res.sendStatus(StatusCodes.NOT_FOUND);
         return;
       }
-      res.json(data);
+      res.json(data.map(({ coupon, rank }) => ({ coupon, rank })));
+    } catch (e) {
+      res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+  });
+
+  routes.get("/keys", async (_, res) => {
+    try {
+      const data = await readAll();
+      if (data.length === 0) {
+        res.sendStatus(StatusCodes.NOT_FOUND);
+        return;
+      }
+      res.json(data.map(({ _id }) => _id));
     } catch (e) {
       res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
     }
@@ -55,6 +70,27 @@ const routes = (config: Config) => {
     try {
       const key = await saveCoupon(body.data);
       res.status(StatusCodes.CREATED).send(key);
+    } catch (e) {
+      console.log(e);
+      res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+  });
+
+  routes.post("/many", async (req, res) => {
+    const body = couponArraySchema.safeParse(req.body);
+    if (!body.success) {
+      const message = body.error.issues
+        .map(({ message }) => message)
+        .join("\n");
+      res.status(StatusCodes.BAD_REQUEST).send(message);
+      return;
+    }
+    try {
+      const jobs = body.data.map((coupon) => saveCoupon(coupon));
+      const keys = await Promise.all(jobs);
+      res
+        .status(StatusCodes.CREATED)
+        .json(keys.filter((key) => key !== undefined));
     } catch (e) {
       console.log(e);
       res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
